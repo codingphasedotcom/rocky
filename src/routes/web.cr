f8be9f0fad4fa47bc1e8ee7d82d19a07263c16ba
Joe Santos Garcia <joesantosgarcia@gmail.com>
2017-04-29 22:39:20 -0400

    no message

--- src/routes/web.cr
+++ src/routes/web.cr
@@ -1,9 +1,14 @@
-
-
-
 # Matches GET "http://host:port/"
 get "/" do
   render "src/views/pages/homepage.ecr", "src/views/layouts/main.ecr"
+end
+
+get "/layout-test" do
+  render "src/views/pages/testingpage.ecr", "src/views/layouts/testinglayout.ecr"
+end
+
+get "/contact" do
+  render "src/views/pages/contactpage.ecr", "src/views/layouts/testinglayout.ecr"
 end
 
 get "/protect" do |env|
