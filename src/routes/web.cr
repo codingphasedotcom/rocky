c8be144d1715bee6e0d2bb21fd310211e85a0706
Joe Santos Garcia <joesantosgarcia@gmail.com>
2017-04-29 23:23:12 -0400

    no message

--- src/routes/web.cr
+++ src/routes/web.cr
@@ -1,6 +1,3 @@
-
-
-
 # Matches GET "http://host:port/"
 get "/" do
   render "src/views/pages/homepage.ecr", "src/views/layouts/main.ecr"
@@ -8,9 +5,4 @@ end
 
 get "/protect" do |env|
   env.response.status_code = 403
-end
-
-get "/user" do
-  name = "joe"
-  render "src/views/pages/user.ecr", "src/views/layouts/main.ecr"
 end
