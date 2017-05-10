cb347a8bcb0708a89f00a06f5362643e3f93c246
Joe Santos Garcia <joesantosgarcia@gmail.com>
2017-05-09 22:52:13 -0400

    setup database

--- src/config/database.cr
+++ src/config/database.cr
@@ -1,4 +1,4 @@
 require "mysql"
 
-MYSQL = DB.open("mysql://root:root@127.0.0.1:3306/movies")
-at_exit { MYSQL.close }
+# MYSQL = DB.open("mysql://root:root@127.0.0.1:3306/movies")
+# at_exit { MYSQL.close }
--- src/routes/web.cr
+++ src/routes/web.cr
@@ -2,23 +2,3 @@
 get "/" do
   render "src/views/pages/homepage.ecr", "src/views/layouts/main.ecr"
 end
-
-get "/protect" do |env|
-  env.response.status_code = 403
-end
-
-get "/movies" do
-  MYSQL.exec "
-      CREATE TABLE IF NOT EXISTS movies
-      (
-        id int not null auto_increment,
-        title varchar(30),
-        year int,
-        description LONGTEXT,
-        primary key (id)
-      )"
-
-  MYSQL.exec "
-  INSERT INTO movies (title, year, description)
-VALUES ('Star Trek', '1996', 'A SHITTY MOVIE');"
-end
